9487c4c56fdaf6d255b347dba108b0812d508b8d
Annabelle Coles <59674595+annabellecoles@users.noreply.github.com>
2023-05-25 16:58:21 -0700

    Update low_dimensionality.py

--- low_dimensionality.py
+++ low_dimensionality.py
@@ -1,5 +1,3 @@
-import numpy as np 
-
 low_dim = np.random.randn(len(counts), 2)
 
 def calculate_low_dimension(low_dim):
@@ -12,7 +10,7 @@ def calculate_low_dimension(low_dim):
             if(i == j): numerators[i][j] = 0
             else: 
                 print((1+(low_dim[i] - low_dim[j])**2)**-1)
-                numerators[i][j] = (1+(np.sum(low_dim[i] - low_dim[j])**2))**-1
+                numerators[i][j] = (1+(np.linalg.norm(low_dim[i] - low_dim[j])**2))**-1
                 
         denominators[i]=np.sum(numerators[i,:])
 
