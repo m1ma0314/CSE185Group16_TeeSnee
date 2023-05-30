0e0f5690fe4321e252e71824906a7b3b2d39ea09
m1ma <97704603+m1ma0314@users.noreply.github.com>
2023-05-29 21:26:00 -0700

    Python Parse Command-line Arguments Tools
    Need to fix how to use tsne function after called

---
+++ my_tsne.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""
+Command-line script to run t-SNE
+"""
+
+import argparse
+import pandas as pd
+
+def main():
+  parser = argparse.ArgumentParser(
+      prog="tsne.py",
+      description="command-line script to generate tsne plots"
+  )
+
+  # Input
+  parser.add_argument("filename", help="gene data file (specify if zipped or not)",type=str)
+  parser.add_argument("-p","--target_perplexity",help="user specificed perplexity",type=int,metavar="PERPLEXITY",required=True)
+  parser.add_argument("-z","--zipped",help="unzip file if input is zipped",action="store_true")
+
+  args = parser.parse_args()
+  if args.zipped:
+      print("File is zipped. Extracting and reading as CSV:", args.filename)
+      unzipped_filename = args.filename
+      unzipped_filename = pd.read_csv(unzipped_filename,compression='gzip', delimiter='\t')
+      tsne(unzipped_filename,args.target_perplexity)
+  else:
+      tsne(args.filename,args.target_perplexity)
+
+
+if __name__ == "__main__":
+  main()
